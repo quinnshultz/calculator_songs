754ea40129bb6487621e741be0e9bf60d2c4bdcf
Quinn Shultz <vanoxite@gmail.com>
2022-10-21 15:55:37 -0400

    Moved main() and play_game() into new module.

---
+++ c/doom/doom.c
@@ -0,0 +1,95 @@
+// Doom for HP50G
+// Written by Quinn Shultz
+
+#include <hpgcc49.h>
+#include <hpgraphics.h>
+
+#include "texture.h"
+
+// TODO: Move to "doom.c"
+int play_game()
+{
+    sys_slowOff();  // Run the game at full system speed
+
+    hpg_set_mode_gray16(0);
+    hpg_clear();
+
+    // Main gameplay loop
+    while (!keyb_isON());
+
+    sys_slowOn();   // Save batteries in the main menu
+    return 0;
+}
+
+// TODO: Move to "doom.c"
+int main()
+{
+    hpg_set_mode_gray16(0); //enter 16 colour, single buffered mode
+    hpg_clear(); //clear the screen
+
+    enum menu_items { play, settings, about, extras };
+    enum menu_items selection = play;
+
+    hpg_t* background = main_menu();
+    hpg_t* cursor = main_menu_cursor();
+
+    hpg_blit(background, 0, 0, 131, 80, hpg_stdscreen, 0, 0);
+    hpg_blit(cursor, 0, 0, 16, 16, hpg_stdscreen, 10, 65);
+    while (!keyb_isON())
+    {
+        if (keyb_isLeft() && selection > 0)
+        {
+            selection--;
+            // Is it really necessary to redraw the whole screen every time?
+            hpg_clear();
+            hpg_blit(background, 0, 0, 131, 80, hpg_stdscreen, 0, 0);
+            hpg_blit(cursor, 0, 0, 16, 16, hpg_stdscreen, (35 * selection) + 10, 65);
+        }
+        else if (keyb_isLeft())
+        {
+            selection = extras;
+            hpg_clear();
+            hpg_blit(background, 0, 0, 131, 80, hpg_stdscreen, 0, 0);
+            hpg_blit(cursor, 0, 0, 16, 16, hpg_stdscreen, (35 * selection) + 10, 65);
+        }
+        if (keyb_isRight() && selection < 3)
+        {
+            selection++;
+            hpg_clear();
+            hpg_blit(background, 0, 0, 131, 80, hpg_stdscreen, 0, 0);
+            hpg_blit(cursor, 0, 0, 16, 16, hpg_stdscreen, (35 * selection) + 10, 65);
+        }
+        else if (keyb_isRight())
+        {
+            selection = play;
+            hpg_clear();
+            hpg_blit(background, 0, 0, 131, 80, hpg_stdscreen, 0, 0);
+            hpg_blit(cursor, 0, 0, 16, 16, hpg_stdscreen, (35 * selection) + 10, 65);
+        }
+
+        // User makes a selection with the "ENTER" key
+        if (keyb_isKeyPressed(0, 6))
+        {
+            switch (selection) {
+            case play:
+                play_game();
+                break;
+            case settings:
+                sat_stack_push_string("Settings");
+                break;
+            case about:
+                sat_stack_push_string("About");
+                break;
+            case extras:
+                sat_stack_push_string("Extras");
+                break;
+            }
+
+        }
+
+
+    }
+    hpg_free_image(cursor);
+    hpg_free_image(background);
+    return 0;
+}
--- c/doom/texture.h
+++ c/doom/texture.h
@@ -1,6 +1,10 @@
 // This module generates textures proceedingly for use in video games.
 // Written by Quinn Shultz
+#ifndef _TEXTURE_H_
+#define _TEXTURE_H_
 
 hpg_t* main_menu();
 hpg_t* main_menu_cursor();
 hpg_t* draw_hud();
+
+#endif
